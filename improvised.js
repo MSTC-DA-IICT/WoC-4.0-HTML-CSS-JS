90e81e922af8868ed4818a8a7e87d7762a90a39e
Jinal Prajapati <71439355+Jinal-Prajapati@users.noreply.github.com>
2021-12-23 01:48:43 +0530

    Update improvised.js

--- improvised.js
+++ improvised.js
@@ -14,46 +14,10 @@ const lis = document.getElementsByClassName('lis');
 const delList = document.getElementsByClassName('deleteList');
 const tasks = document.getElementsByTagName('span');
 
-
 // EVENT LISTENERS
 cards.addEventListener('click', doit);
 newList.addEventListener('click', addNewList);
 
-// FUNCTIONS
-
-// 1. Populate the DOM using the storage - HARSH
-
-// 2. Add new list - JINAL
-/* `<div class="card">
-    <div class="cardTitle">
-        <h2>Entertainment</h2>
-    </div>
-    <div class="tasks">
-        <ul>
-            
-        </ul>
-        <button class="newTask">Add Task</button>
-        <button class="deleteList">Delete List</button>
-    </div>
-</div>` */
-
-// 3. Add new task to a list - KAUSHAL
-/* `<li class="lis">
-        <span>
-            <i class="fas fa-trash"></i>
-        </span>
-    </li>` */
-
-// 4. Delete a task from the list - ARPAN
-// 5. Delete a list - ISHA
-// 6. Strike through a task(mark as done) - KAVYA
-// 7. Update a task - DHAIRYA
-
-
-
-
-
-// 
 function doit(e) {
     if (e.target.className === 'newTask') {
         const usertask = prompt("enter the task");
@@ -82,38 +46,22 @@ function doit(e) {
         e.target.classList.toggle('done');
 }
 
-function addNewList() {
+function addNewList()
+{
     const userTitle = prompt('Enter the title of your new list');
 
-    const card = document.createElement('div');
-    card.classList.add('card');
+    let HTMLstr = `<div class="card">
+                    <div class="cardTitle">
+                        <h2>${userTitle}</h2>
+                    </div>
+                    <div class="tasks">
+                        <ul>
+                            
+                        </ul>
+                        <button class="newTask">Add Task</button>
+                        <button class="deleteList">Delete List</button>
+                    </div>`
 
-    const cardTitle = document.createElement('div');
-    cardTitle.classList.add('cardTitle');
-
-    const cardTitleh2 = document.createElement('h2');
-    cardTitleh2.innerText = userTitle;
-
-    const tasksdiv = document.createElement('div');
-    tasksdiv.classList.add('tasks');
-
-    const tasksul = document.createElement('ul');
-    const createTaskBtn = document.createElement('button');
-    const deleteListBtn = document.createElement('button');
-
-    createTaskBtn.classList.add('newTask');
-    createTaskBtn.innerText = 'Add Task';
-    deleteListBtn.classList.add('deleteList');
-    deleteListBtn.innerText = 'Delete List';
-
-    cards.appendChild(card);
-
-    card.appendChild(cardTitle);
-    card.appendChild(tasksdiv);
-
-    cardTitle.appendChild(cardTitleh2);
-
-    tasksdiv.appendChild(tasksul);
-    tasksdiv.appendChild(createTaskBtn);
-    tasksdiv.appendChild(deleteListBtn);
+    const div = document.getElementById('cards');
+    div.innerHTML += HTMLstr;
 }
